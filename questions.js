1119650daa81c708cf974b1acab46b07ab351484
Aditya Shrikant Gangwar <166237194+aadishri00@users.noreply.github.com>
2024-12-24 22:04:12 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,82 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following attributes is used to add link to any element?",
+    answer: "href",
+    options: [
+      "link",
+      "href",
+      "ref",
+      "newref"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Markup Language",
+      "Hyper Text Preprocessor",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is Rgb stand for?",
+    answer: "red green blue",
+    options: [
+      "right green board",
+      "red green blue",
+      "red gray brown",
+      "red gray black"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which is not markup language?",
+    answer: "Markdown",
+    options: [
+      "HTML",
+      "XML",
+      "Java",
+      "Markdown"
+    ]
+  }
+  ,
+  {
+    numb: 7,
+    question: "what is the use of the css?",
+    answer: "Design the web page",
+    options: [
+      "for layout of the web page",
+      "for running the web page",
+      "Design the web page",
+      "To make a web page dynamic"
+    ]
+  }
+
+
+];
